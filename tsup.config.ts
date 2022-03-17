e2f4fd9c093c9536c646f0e53ac06d309ff2d564
三咲智子 <sxzz@sxzz.moe>
2022-03-17 21:59:47 +0800

    chore: add dev

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'tsup'
 
+const isDev = process.env.MODE === 'dev'
+
 export default defineConfig({
   entry: ['./src/**/*.ts'],
   format: ['cjs', 'esm'],
   target: 'node12',
   splitting: true,
   sourcemap: true,
-  dts: true,
+  dts: !isDev,
+  watch: isDev,
 })
