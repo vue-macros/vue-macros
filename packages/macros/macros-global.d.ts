d6e8533a727b93c0de556aef82cd11106f5a587e
三咲智子 <sxzz@sxzz.moe>
2022-08-26 00:58:41 +0800

    fix(macros): dts path

--- packages/macros/macros-global.d.ts
+++ packages/macros/macros-global.d.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/triple-slash-reference */
-/// <reference types="@vue-macros/define-model/macros" />
+/// <reference types="@vue-macros/define-model/macros-global" />
 /// <reference types="unplugin-vue-define-options" />
-/// <reference types="@vue-macros/define-render/macros" />
-/// <reference types="@vue-macros/setup-component/macros" />
+/// <reference types="@vue-macros/define-render/macros-global" />
+/// <reference types="@vue-macros/setup-component/macros-global" />
 
 export {}
