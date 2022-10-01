dfa907feb7e7305726d028e9e502c1206cc9ccb0
snowingfox <1503401882@qq.com>
2022-10-01 23:05:43 +0800

    feat: update playground

--- playground/src/demo/setup-fc.type.tsx
+++ playground/src/demo/setup-fc.type.tsx
@@ -1,16 +1,23 @@
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
+import { a } from './foo'
 
 export const SetupFCType: SetupFC = () => {
   const count = ref(0)
+  const double = computed(() => count.value * 2)
 
   return () => (
     <div>
       <h3>SetupFCType</h3>
       <p>count: {count.value}</p>
-      <button onClick={() => {
-        count.value++
-        debugger
-      }}>inc & check sourcemap</button>
+      <p>double: {double.value}</p>
+      <button
+        onClick={() => {
+          count.value++
+          debugger
+        }}
+      >
+        inc & check sourcemap
+      </button>
     </div>
   )
 }
