076e0dc6f266921cbc70591d9685c22931f12305
Hydration <3038094028@qq.com>
2022-10-22 01:44:20 +0800

    docs: support PWA (#114)
    Co-authored-by: 三咲智子 <sxzz@sxzz.moe>

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,63 +1,135 @@
 import { defineConfig } from 'vitepress'
+import { withPwa } from './pwa'
 import { markdownConfig, nav, sidebar } from './configs'
 
-export default defineConfig({
-  lang: 'en-US',
-  title: 'Vue Macros',
-  head: [
-    ['link', { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }],
-    ['meta', { property: 'og:type', content: 'website' }],
-    ['meta', { property: 'og:title', content: 'Vue Macros' }],
-    ['meta', { property: 'og:url', content: 'https://vue-macros.sxzz.moe' }],
-    [
-      'meta',
-      {
-        property: 'og:description',
-        content: 'Explore and extend more macros and syntax sugar to Vue.',
-      },
-    ],
-    ['meta', { name: 'theme-color', content: '#914796' }],
-    [
-      'script',
-      {
-        async: '',
-        src: 'https://www.googletagmanager.com/gtag/js?id=G-29NKGSL23C',
-      },
-    ],
-    [
-      'script',
-      {},
-      `window.dataLayer = window.dataLayer || [];
+export default withPwa(
+  defineConfig({
+    lang: 'en-US',
+    title: 'Vue Macros',
+    head: [
+      ['link', { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }],
+      ['meta', { property: 'og:type', content: 'website' }],
+      ['meta', { property: 'og:title', content: 'Vue Macros' }],
+      ['meta', { property: 'og:url', content: 'https://vue-macros.sxzz.moe' }],
+      [
+        'meta',
+        {
+          property: 'og:description',
+          content: 'Explore and extend more macros and syntax sugar to Vue.',
+        },
+      ],
+      ['meta', { name: 'theme-color', content: '#914796' }],
+      [
+        'script',
+        {
+          async: '',
+          src: 'https://www.googletagmanager.com/gtag/js?id=G-29NKGSL23C',
+        },
+      ],
+      [
+        'script',
+        {},
+        `window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 gtag('config', 'G-29NKGSL23C');`,
+      ],
     ],
-  ],
 
-  description: 'Explore and extend more macros and syntax sugar to Vue.',
-  lastUpdated: true,
-  cleanUrls: 'with-subfolders',
-  markdown: markdownConfig,
+    description: 'Explore and extend more macros and syntax sugar to Vue.',
+    lastUpdated: true,
+    cleanUrls: 'disabled',
+    markdown: markdownConfig,
 
-  vue: {
-    reactivityTransform: true,
-  },
-
-  themeConfig: {
-    logo: '/favicon.svg',
-    footer: {
-      message: 'Made with ❤️',
-      copyright: 'MIT License © 2022 三咲智子',
+    vue: {
+      reactivityTransform: true,
     },
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/sxzz/unplugin-vue-macros' },
-    ],
-    editLink: {
-      pattern:
-        'https://github.com/sxzz/unplugin-vue-macros/edit/main/docs/:path',
-      text: 'Edit this page on GitHub',
+
+    themeConfig: {
+      logo: '/favicon.svg',
+      footer: {
+        message: 'Made with ❤️',
+        copyright: 'MIT License © 2022 三咲智子',
+      },
+      socialLinks: [
+        { icon: 'github', link: 'https://github.com/sxzz/unplugin-vue-macros' },
+      ],
+      editLink: {
+        pattern:
+          'https://github.com/sxzz/unplugin-vue-macros/edit/main/docs/:path',
+        text: 'Edit this page on GitHub',
+      },
+      nav,
+      sidebar,
     },
-    nav,
-    sidebar,
-  },
-})
+    pwa: {
+      outDir: '.vitepress/dist',
+      manifest: {
+        name: 'Vue Macros',
+        short_name: 'Vue Macros',
+        description: 'Explore and extend more macros and syntax sugar to Vue.',
+        theme_color: '#914796',
+        id: '/',
+        icons: [
+          {
+            src: '/favicon.svg',
+            type: 'image/svg+xml',
+            sizes: 'any',
+            purpose: 'maskable any',
+          },
+        ],
+      },
+      devOptions: {
+        enabled: true,
+      },
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{css,js,html,svg,png,ico,txt,woff2}'],
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'google-fonts-cache',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365, // <== 365 days
+              },
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
+          },
+          {
+            urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'gstatic-fonts-cache',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365, // <== 365 days
+              },
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
+          },
+          {
+            urlPattern: /^https:\/\/cdn\.jsdelivr\.net\/.*/i,
+            handler: 'NetworkFirst',
+            options: {
+              cacheName: 'jsdelivr-images-cache',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 7, // <== 7 days
+              },
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
+          },
+        ],
+      },
+    },
+  })
+)
--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -4,5 +4,9 @@ import DefineOptions from 'unplugin-vue-define-options/vite'
 import Jsx from '@vitejs/plugin-vue-jsx'
 
 export default defineConfig({
+  build: {
+    ssrManifest: false,
+    manifest: false,
+  },
   plugins: [Unocss(), DefineOptions(), Jsx()],
 })
