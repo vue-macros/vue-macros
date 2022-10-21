881c466ac87da346a800dc5d7640e54fa5477a4f
Hydrogen <3038094028@qq.com>
2022-10-22 02:19:40 +0800

    chore: optimize icon set

---
+++ docs/.vitepress/pwa/icons.ts
@@ -0,0 +1,112 @@
+export default [
+  {
+    src: '/favicon.svg',
+    type: 'image/svg+xml',
+    sizes: 'any',
+    purpose: 'any',
+  },
+  {
+    src: '/pwa/ios/16.png',
+    sizes: '16x16',
+  },
+  {
+    src: '/pwa/ios/20.png',
+    sizes: '20x20',
+  },
+  {
+    src: '/pwa/ios/29.png',
+    sizes: '29x29',
+  },
+  {
+    src: '/pwa/ios/32.png',
+    sizes: '32x32',
+  },
+  {
+    src: '/pwa/ios/40.png',
+    sizes: '40x40',
+  },
+  {
+    src: '/pwa/ios/50.png',
+    sizes: '50x50',
+  },
+  {
+    src: '/pwa/ios/57.png',
+    sizes: '57x57',
+  },
+  {
+    src: '/pwa/ios/58.png',
+    sizes: '58x58',
+  },
+  {
+    src: '/pwa/ios/60.png',
+    sizes: '60x60',
+  },
+  {
+    src: '/pwa/ios/64.png',
+    sizes: '64x64',
+  },
+  {
+    src: '/pwa/ios/72.png',
+    sizes: '72x72',
+  },
+  {
+    src: '/pwa/ios/76.png',
+    sizes: '76x76',
+  },
+  {
+    src: '/pwa/ios/80.png',
+    sizes: '80x80',
+  },
+  {
+    src: '/pwa/ios/87.png',
+    sizes: '87x87',
+  },
+  {
+    src: '/pwa/ios/100.png',
+    sizes: '100x100',
+  },
+  {
+    src: '/pwa/ios/114.png',
+    sizes: '114x114',
+  },
+  {
+    src: '/pwa/ios/120.png',
+    sizes: '120x120',
+  },
+  {
+    src: '/pwa/ios/128.png',
+    sizes: '128x128',
+  },
+  {
+    src: '/pwa/ios/144.png',
+    sizes: '144x144',
+  },
+  {
+    src: '/pwa/ios/152.png',
+    sizes: '152x152',
+  },
+  {
+    src: '/pwa/ios/167.png',
+    sizes: '167x167',
+  },
+  {
+    src: '/pwa/ios/180.png',
+    sizes: '180x180',
+  },
+  {
+    src: '/pwa/ios/192.png',
+    sizes: '192x192',
+  },
+  {
+    src: '/pwa/ios/256.png',
+    sizes: '256x256',
+  },
+  {
+    src: '/pwa/ios/512.png',
+    sizes: '512x512',
+  },
+  {
+    src: '/pwa/ios/1024.png',
+    sizes: '1024x1024',
+  },
+]
