b6063725ac1ba3de56ae78044fc818b9e12c0881
三咲智子 <sxzz@sxzz.moe>
2022-11-04 14:33:15 +0800

    chore(playground): add type test

--- playground/vue3/src/examples/define-props/child.vue
+++ playground/vue3/src/examples/define-props/child.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
+import { expectTypeOf } from 'expect-type'
+import type { Ref } from 'vue'
+
 const { foo } = $defineProps<{
   foo: string[]
 }>()
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const fooRef = $$(foo)
+expectTypeOf(fooRef).toEqualTypeOf<Ref<string[]>>()
 </script>
 
 <template>
