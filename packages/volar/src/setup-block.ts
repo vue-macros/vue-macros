6979522985fda3aa5a79ffa7c2a7bd9158d8ba0c
三咲智子 <sxzz@sxzz.moe>
2022-11-23 10:37:37 +0800

    wip: volar setup block

---
+++ packages/volar/src/setup-block.ts
@@ -0,0 +1,10 @@
+import type { VueLanguagePlugin } from '@volar/vue-language-core'
+
+const plugin: VueLanguagePlugin = () => {
+  return {
+    name: 'vue-macros-setup-block',
+    version: 1,
+  }
+}
+
+export default plugin
--- playground/vue3/src/examples/named-template/index.vue
+++ playground/vue3/src/examples/named-template/index.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 import Child from './child.vue'
 import Test from './test.vue'
+
+const o = { [1]: 2 }
 </script>
 
 <template>
+  <div v-for="k in o" />
+
   <child title="title" unit="px" unit-is-right />
 
   <hr />
