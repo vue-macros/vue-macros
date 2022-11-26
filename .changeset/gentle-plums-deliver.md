28644c8681dbd34d0983c29186052469b999db74
三咲智子 <sxzz@sxzz.moe>
2022-11-26 13:17:27 +0800

    fix(macros): plugin order
    closes #166

---
+++ .changeset/gentle-plums-deliver.md
@@ -0,0 +1,5 @@
+---
+'unplugin-vue-macros': patch
+---
+
+fix plugin order
--- playground/vue3/vite.config.ts
+++ playground/vue3/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   },
   plugins: [
     VueMacros({
-      // setupComponent: false,
+      setupBlock: true,
       plugins: {
         vue: Vue({
           include: [/\.vue$/, /setup\.[cm]?[jt]sx?$/],
