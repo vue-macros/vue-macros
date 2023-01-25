a3accd53e8a1251aaffa2cd7fe793c01d2144376
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2023-01-26 01:37:32 +0800

    test(reactivity-transform): add vue 3 fixtures

--- packages/reactivity-transform/tests/fixtures.test.ts
+++ packages/reactivity-transform/tests/fixtures.test.ts
@@ -4,6 +4,7 @@ import {
   RollupEsbuildPlugin,
   RollupEscapeNullCharacterPlugin,
   RollupRemoveVueFilePathPlugin,
+  RollupVue,
   RollupVue2,
   rollupBuild,
   testFixtures,
@@ -12,19 +13,32 @@ import VueReactivityTransform from '../src/rollup'
 
 describe('fixtures', async () => {
   await testFixtures(
-    'tests/fixtures/*.{vue,js,ts}',
-    (args, id) =>
-      rollupBuild(id, [
-        VueReactivityTransform(),
-        RollupVue2({
-          compiler: require('vue2/compiler-sfc'),
-        }),
-        RollupRemoveVueFilePathPlugin(),
-        RollupEscapeNullCharacterPlugin(),
-        RollupEsbuildPlugin({
-          target: 'esnext',
-        }),
-      ]),
+    'tests/fixtures/**/*.{vue,js,ts}',
+    (args, id) => {
+      if (id.includes('vue2'))
+        return rollupBuild(id, [
+          VueReactivityTransform(),
+          RollupVue2({
+            compiler: require('vue2/compiler-sfc'),
+          }),
+          RollupRemoveVueFilePathPlugin(),
+          RollupEscapeNullCharacterPlugin(),
+          RollupEsbuildPlugin({
+            target: 'esnext',
+          }),
+        ])
+      else
+        return rollupBuild(id, [
+          VueReactivityTransform(),
+          RollupVue({
+            compiler: require('vue/compiler-sfc'),
+          }),
+          RollupRemoveVueFilePathPlugin(),
+          RollupEsbuildPlugin({
+            target: 'esnext',
+          }),
+        ])
+    },
     {
       cwd: resolve(__dirname, '..'),
       promise: true,
