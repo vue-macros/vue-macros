698ff11a99034a510065397641afc22f0f8f7041
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2023-01-26 16:21:38 +0800

    test(reactivity-transform): add default value case

---
+++ packages/reactivity-transform/tests/fixtures/vue3/default-value.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const {
+  fn = () => {
+    console.log('hello')
+  },
+  obj = (() => ({ foo: 'bar' })) as never,
+} = defineProps<{
+  fn: () => void
+  obj: Record<string, string>
+}>()
+
+console.log(fn, obj)
+</script>
