869fd5fbdfbbb7c73269744674b95dd1fb0abded
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2023-01-28 05:18:13 +0800

    test(define-props-refs): fix fixtures type

--- packages/define-props-refs/tests/fixtures/basic.vue
+++ packages/define-props-refs/tests/fixtures/basic.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
 import { expectTypeOf } from 'expect-type'
-import type { Ref } from 'vue'
+import type { DeepReadonly, Ref } from 'vue'
 
 const { foo, bar } = definePropsRefs<{
   foo: string[]
   bar: number
 }>()
-expectTypeOf(foo).toEqualTypeOf<Ref<string[]>>()
-expectTypeOf(bar).toEqualTypeOf<Ref<number>>()
+expectTypeOf(foo).toEqualTypeOf<DeepReadonly<Ref<string[]>>>()
+expectTypeOf(bar).toEqualTypeOf<DeepReadonly<Ref<number>>>()
 </script>
 
 <template>
