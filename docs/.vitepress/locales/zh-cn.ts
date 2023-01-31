724891945fa902e5ab0bd117b872b7abc941551c
liangzhensheng <1250564179@qq.com>
2023-01-31 15:15:40 +0800

    fix: add sidebar type assertion

--- docs/.vitepress/locales/zh-cn.ts
+++ docs/.vitepress/locales/zh-cn.ts
@@ -10,7 +10,7 @@ export const nav: DefaultTheme.NavItem[] = [
   { text: '特性', link: '/zh-CN/features/hoist-static' },
 ]
 
-export const sidebar = common.sidebar('zh-CN')
+export const sidebar = common.sidebar('zh-CN') as DefaultTheme.SidebarGroup[]
 
 const sidebarTitle: string[] = ['指南', '宏', '特性']
 const sidebarItem: string[][] = [
