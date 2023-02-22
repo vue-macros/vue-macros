cb525c6bc2a7703323f21ffbce4037f8540143fa
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2023-02-23 03:55:38 +0800

    test(reactivity-transform): add array default value demo

--- packages/reactivity-transform/tests/fixtures/vue3/default-value.vue
+++ packages/reactivity-transform/tests/fixtures/vue3/default-value.vue
@@ -4,10 +4,12 @@ const {
     console.log('hello')
   },
   obj = (() => ({ foo: 'bar' })) as never,
+  arr = (() => ['foo', 'bar']) as never,
 } = defineProps<{
   fn: () => void
   obj: Record<string, string>
+  arr: string[]
 }>()
 
-console.log(fn, obj)
+console.log(fn, obj, arr)
 </script>
