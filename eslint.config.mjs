e9413b3f63d437958342c2233758e45f88b585dd
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2023-03-23 06:49:03 +0800

    chore: switch to eslint flat config

---
+++ eslint.config.mjs
@@ -0,0 +1,31 @@
+import { sxzz } from '@sxzz/eslint-config'
+
+export default sxzz([
+  {
+    ignores: ['playground/vue2', 'playground/nuxt', '**/helper/*.js'],
+  },
+  {
+    files: ['**/*.vue'],
+    rules: {
+      'vue/valid-attribute-name': 'off',
+      'vue/no-export-in-script-setup': 'off',
+    },
+  },
+  {
+    files: ['**/*.md/*.{js,ts,vue}'],
+    rules: {
+      'import/no-mutable-exports': 'off',
+      '@typescript-eslint/no-inferrable-types': 'off',
+    },
+  },
+  {
+    files: ['playground/vue3/**'],
+    rules: {
+      'no-debugger': 'off',
+      'no-console': 'off',
+      'vue/require-prop-types': 'off',
+      'vue/valid-define-props': 'off',
+      'vue/valid-attribute-name': 'off',
+    },
+  },
+])
