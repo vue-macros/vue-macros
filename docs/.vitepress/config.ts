9cd2181b5818d97203c061aa1995f0dd1d0c69f1
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2023-04-16 13:38:45 +0800

    docs: switch to built-in search

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -11,6 +11,25 @@ export default withPwa(
       root: en,
       'zh-CN': zhCN,
     },
+    themeConfig: {
+      search: {
+        provider: 'local',
+        options: {
+          locales: {
+            'zh-CN': {
+              translations: {
+                button: { buttonText: '搜索文档', buttonAriaLabel: '搜索文档' },
+                modal: {
+                  noResultsText: '无法找到相关结果',
+                  resetButtonTitle: '清除查询条件',
+                  footer: { selectText: '选择', navigateText: '切换' },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
     pwa,
   })
 )
--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -2,14 +2,6 @@ import { defineConfig } from 'vite'
 import Unocss from 'unocss/vite'
 import VueMacros from 'unplugin-vue-macros/vite'
 import VueJsx from '@vitejs/plugin-vue-jsx'
-// @ts-expect-error
-import { SearchPlugin } from 'vitepress-plugin-search'
-
-const searchOptions = {
-  previewLength: 62,
-  buttonLabel: 'Search',
-  placeholder: 'Search docs',
-}
 
 export default defineConfig({
   build: {
@@ -23,7 +15,6 @@ export default defineConfig({
         vueJsx: VueJsx(),
       },
     }),
-    SearchPlugin(searchOptions),
     Unocss(),
   ],
 })
