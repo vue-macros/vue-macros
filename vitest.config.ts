e7f2b0cb8c78986c81433c79f3b4cfb35eb559e4
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2023-04-16 16:06:07 +0800

    ci: drop node 14

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,18 +1,10 @@
 import { defineConfig } from 'vitest/config'
-import esbuild from 'rollup-plugin-esbuild'
 
 export default defineConfig(() => {
   return {
     resolve: {
       conditions: ['dev'],
     },
-    plugins: [
-      process.version.startsWith('v14')
-        ? esbuild({
-            target: 'node14',
-          })
-        : [],
-    ],
     test: {
       singleThread: true,
     },
