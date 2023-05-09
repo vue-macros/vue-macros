30efea50c634bacb84c7d1350824fa7082efcc57
Hooray Hu <304327508@qq.com>
2023-05-10 01:42:13 +0800

    feat(docs): add search panel translation (#361)

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -22,7 +22,11 @@ export default withPwa(
                 modal: {
                   noResultsText: '无法找到相关结果',
                   resetButtonTitle: '清除查询条件',
-                  footer: { selectText: '选择', navigateText: '切换' },
+                  footer: {
+                    selectText: '选择',
+                    navigateText: '切换',
+                    closeText: '关闭',
+                  },
                 },
               },
             },
