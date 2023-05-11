5bbffe92a5816fa614d0587842a3ae81fb15855c
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2023-05-11 21:17:32 +0800

    refactor(docs): remove unplugin-vue-macros

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import Unocss from 'unocss/vite'
-import VueMacros from 'unplugin-vue-macros/vite'
 import VueJsx from '@vitejs/plugin-vue-jsx'
 
 export default defineConfig({
@@ -9,12 +8,5 @@ export default defineConfig({
     ssrManifest: false,
     manifest: false,
   },
-  plugins: [
-    VueMacros({
-      plugins: {
-        vueJsx: VueJsx(),
-      },
-    }),
-    Unocss(),
-  ],
+  plugins: [VueJsx(), Unocss()],
 })
