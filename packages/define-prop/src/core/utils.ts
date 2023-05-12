e04e7d845410d31b98c34636edd6ae7ca9441161
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2023-05-12 17:46:58 +0800

    fix(define-prop): TS error

--- packages/define-prop/src/core/utils.ts
+++ packages/define-prop/src/core/utils.ts
@@ -4,7 +4,7 @@ import { type CallExpression, type Node, type TSType } from '@babel/types'
 export type Impl = (ctx: {
   s: MagicString
   offset: number
-  resolveTSType(type: TSType): Promise<string[]>
+  resolveTSType(type: TSType): Promise<string[] | undefined>
 }) => {
   walkCall(node: CallExpression, parent: Node): string
   genRuntimeProps(isProduction: boolean): Promise<string | undefined>
