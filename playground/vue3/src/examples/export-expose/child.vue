cfa1fc4f7fe4a4e46568d421389ea5e2a7d50235
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2023-05-22 18:56:17 +0800

    feat(playground): add export expose feature

---
+++ playground/vue3/src/examples/export-expose/child.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+export const foo = 'foo'
+export const bar = 'baz'
+</script>
+
+<template>exportExpose child.</template>
---
+++ playground/vue3/src/examples/export-expose/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import Child from './child.vue'
+
+const child = ref()
+</script>
+
+<template>
+  <Child ref="child" />
+  {{ child }}
+</template>
