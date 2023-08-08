0fdfa1881b8636c48edcc57336664b1047555012
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2023-08-08 23:28:23 +0800

    docs: translate home page

--- docs/.vitepress/components/HomePage.vue
+++ docs/.vitepress/components/HomePage.vue
@@ -1,27 +1,40 @@
 <script setup lang="ts">
+import { useData } from 'vitepress'
 import { VPTeamMembers } from 'vitepress/theme'
+import { computed } from 'vue'
 
-const members = [
+const data = useData()
+
+const members = computed(() => [
   {
     avatar: 'https://www.github.com/sxzz.png',
     name: '三咲智子 Kevin Deng',
-    title: 'Author',
+    title: data.lang.value === 'zh-CN' ? '作者' : 'Author',
     links: [
       { icon: 'github', link: 'https://github.com/sxzz' },
       { icon: 'twitter', link: 'https://twitter.com/sanxiaozhizi' },
     ],
   },
-]
+])
 </script>
 
 <template>
   <div flex="~ col wrap" items-center mt8>
     <VPTeamMembers size="small" :members="members" />
 
-    <h2 text="center gray xl" mt-11 pb-2>Sponsored by</h2>
+    <h2 text="center gray xl" mt-11 pb-2>
+      {{ data.lang.value === 'zh-CN' ? '赞助者' : 'Sponsored by' }}
+    </h2>
     <img src="https://cdn.jsdelivr.net/gh/sxzz/sponsors/sponsors.wide.svg" />
 
-    <h2 text="center lg" font-bold my-5>💕 Thanks to all the contributors!</h2>
+    <h2 text="center lg" font-bold my-5>
+      💕
+      {{
+        data.lang.value === 'zh-CN'
+          ? '感谢所有的赞助者！'
+          : 'Thanks to all the contributors!'
+      }}
+    </h2>
 
     <a href="https://github.com/vue-macros/vue-macros/graphs/contributors">
       <img src="https://contrib.rocks/image?repo=vue-macros/vue-macros" />
