ff6f0908e6510191c8730b2227305144339379d9
gaokefei <260480378@qq.com>
2023-08-16 20:13:22 +0800

    docs: add v-slot

--- packages/jsx-directive/tests/fixtures/v-slot/index.vue
+++ packages/jsx-directive/tests/fixtures/v-slot/index.vue
@@ -1,12 +1,12 @@
 <script setup lang="tsx">
 import Child from './child.vue'
 
-defineRender(() => <div>
-  <Child v-slot:bottom={{foo}}>
-    {foo}
-    <Child v-slot>
-      default
+defineRender(() => (
+  <div>
+    <Child v-slot:bottom={{ foo }}>
+      {foo}
+      <Child v-slot>default</Child>
     </Child>
-  </Child>
-</div>)
+  </div>
+))
 </script>
