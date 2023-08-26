1a40ce3819e76548f85bd1d0d02d1c23d4658871
gaokefei <260480378@qq.com>
2023-08-26 09:27:54 +0800

    refactor: move export statement to api.ts

--- packages/export-props/src/index.ts
+++ packages/export-props/src/index.ts
@@ -10,8 +10,6 @@ import {
 } from '@vue-macros/common'
 import { transformExportProps } from './core'
 
-export { transformExportProps } from './core'
-
 export type Options = BaseOptions
 export type OptionsResolved = MarkRequired<Options, 'include' | 'version'>
 
