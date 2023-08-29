fadcea72306cf2e5af933321cf18710cb4d1ff87
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2023-08-29 20:30:40 +0800

    test: configure macros

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,18 @@
+import path from 'node:path'
 import { defineConfig } from 'vitest/config'
+import Macros from 'unplugin-macros/vite'
 
 export default defineConfig(() => {
   return {
     resolve: {
+      alias: {
+        '#macros': path.resolve(__dirname, 'macros/index.ts'),
+      },
       conditions: ['dev'],
     },
     test: {
       singleThread: true,
     },
+    plugins: [Macros()],
   }
 })
