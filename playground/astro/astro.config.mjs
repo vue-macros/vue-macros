a4ea6b84d103c10c539959b1431da1f486374743
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2023-08-30 03:30:48 +0800

    refactor(playground): remove manually add template transformer

--- playground/astro/astro.config.mjs
+++ playground/astro/astro.config.mjs
@@ -1,18 +1,12 @@
 import { defineConfig } from 'astro/config'
 import Vue from '@astrojs/vue'
 import Macros from '@vue-macros/astro'
-import { transformShortVmodel } from '@vue-macros/short-vmodel'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     Vue({
       jsx: true,
-      template: {
-        compilerOptions: {
-          nodeTransforms: [transformShortVmodel()],
-        },
-      },
     }),
     Macros(),
   ],
