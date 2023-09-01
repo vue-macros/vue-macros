f6c25ca13863a3f9a3d2dc8f3f0b5e97702b8979
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2023-09-01 14:43:50 +0800

    test: run on vm context

--- vitest.config.ts
+++ vitest.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(() => {
       conditions: ['dev'],
     },
     test: {
-      singleThread: true,
+      experimentalVmThreads: true,
     },
     plugins: [Macros()],
   }
