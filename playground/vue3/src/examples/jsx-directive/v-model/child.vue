9952a755a2a68c72cbbad17af7c0345d305f4764
gaokefei <260480378@qq.com>
2023-09-02 03:20:15 +0800

    feat: add v-model plugin

---
+++ playground/vue3/src/examples/jsx-directive/v-model/child.vue
@@ -0,0 +1,15 @@
+<script setup lang="tsx">
+const slots = defineSlots<{
+  default: (scope: { value: string }) => any
+  title: (scope: { value: string; 'onUpdate:value': (e: any) => void }) => any
+}>()
+
+const value = $ref('')
+defineRender(() => (
+  <>
+    <slots.title v-model_trim={[value, 'value']}></slots.title>
+
+    <slots.default value={value}></slots.default>
+  </>
+))
+</script>
---
+++ playground/vue3/src/examples/jsx-directive/v-model/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="tsx">
+import Child from './child.vue'
+
+defineRender(() => (
+  <Child>
+    <template v-slot:title={{ value, ...emits }}>
+      <input
+        value={value}
+        onInput={(e: any) => emits['onUpdate:value'](e.target.value)}
+      />
+      {{ emits }}
+    </template>
+
+    <template v-slot={{ value }}>{value}</template>
+  </Child>
+))
+</script>
