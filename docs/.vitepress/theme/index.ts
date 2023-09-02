3ea06ba9c4812c6e7c5c1cd2dfd74c2e9f0b19e0
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2023-09-03 06:46:42 +0800

    feat(docs): use view transitions api

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,19 +1,14 @@
-import { h } from 'vue'
 import { type EnhanceAppContext } from 'vitepress'
 import Theme from 'vitepress/theme'
-import HomePage from '../components/HomePage.vue'
 import WarnBadge from '../components/WarnBadge.vue'
 import StabilityLevel from '../components/StabilityLevel.vue'
+import Layout from './Layout.vue'
 import 'uno.css'
 import './style.css'
 
 export default {
   ...Theme,
-  Layout() {
-    return h(Theme.Layout, null, {
-      'home-features-after': () => h(HomePage),
-    })
-  },
+  Layout,
   enhanceApp({ app }: EnhanceAppContext) {
     app.component('WarnBadge', WarnBadge)
     app.component('StabilityLevel', StabilityLevel)
