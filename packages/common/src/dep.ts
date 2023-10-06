4a2a988f7ff507ed17160d0884cd0732469b85a5
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2023-10-06 19:15:10 +0800

    refactor(common): upgrade local-pkg
    Fixes local-pkg paths

--- packages/common/src/dep.ts
+++ packages/common/src/dep.ts
@@ -1,7 +1,15 @@
+import path from 'node:path'
+import { statSync } from 'node:fs'
 import { getPackageInfoSync } from 'local-pkg'
 
 export function detectVueVersion(root: string = process.cwd()): number {
-  const vuePkg = getPackageInfoSync('vue', { paths: [root] })
+  let isFile = false
+  try {
+    isFile = statSync(root).isFile()
+  } catch {}
+  const paths: string[] = [root]
+  if (!isFile) paths.unshift(path.resolve(root, '_index.js'))
+  const vuePkg = getPackageInfoSync('vue', { paths })
   if (vuePkg && vuePkg.version) {
     const version = Number.parseFloat(vuePkg.version)
     return version >= 2 && version < 3 ? Math.trunc(version) : version
