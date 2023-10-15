81eb3a1164f36ce761ab8eae299c451c11ab386f
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2023-10-15 15:40:17 +0800

    fix(playground): vue2 root element

--- playground/vue2/src/examples/define-slots/child.vue
+++ playground/vue2/src/examples/define-slots/child.vue
@@ -15,21 +15,23 @@ defineSlots<Slots>()
 </script>
 
 <template>
-  <slot name="title" foo="foo"><Ok /></slot>
-  <slot name="default" bar="bar"><Fail /></slot>
+  <div>
+    <slot name="title" foo="foo"><Ok /></slot>
+    <slot name="default" bar="bar"><Fail /></slot>
 
-  <!-- For type check -->
-  <!-- eslint-disable-next-line vue/no-constant-condition -->
-  <template v-if="false">
-    <slot name="title" :foo="false" />
-    <slot name="default" :bar="1" />
+    <!-- For type check -->
+    <!-- eslint-disable-next-line vue/no-constant-condition -->
+    <template v-if="false">
+      <slot name="title" :foo="false" />
+      <slot name="default" :bar="1" />
 
-    <!-- @vue-expect-error wrong type -->
-    <slot name="title" :foo="123" />
+      <!-- @vue-expect-error wrong type -->
+      <slot name="title" :foo="123" />
 
-    <!-- @vue-expect-error missing scope -->
-    <slot name="default" />
-    <!-- @vue-expect-error wrong type -->
-    <slot name="default" bar="baz" />
-  </template>
+      <!-- @vue-expect-error missing scope -->
+      <slot name="default" />
+      <!-- @vue-expect-error wrong type -->
+      <slot name="default" bar="baz" />
+    </template>
+  </div>
 </template>
