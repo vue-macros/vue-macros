f3c68c830c16373532c0953c649017073eed1250
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2023-11-15 01:31:11 +0800

    test: disable threads

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,17 +2,16 @@ import path from 'node:path'
 import { defineConfig } from 'vitest/config'
 import Macros from 'unplugin-macros/vite'
 
-export default defineConfig(() => {
-  return {
-    resolve: {
-      alias: {
-        '#macros': path.resolve(__dirname, 'macros/index.ts'),
-      },
-      conditions: ['dev'],
+export default defineConfig({
+  resolve: {
+    alias: {
+      '#macros': path.resolve(__dirname, 'macros/index.ts'),
     },
-    test: {
-      experimentalVmThreads: true,
-    },
-    plugins: [Macros()],
-  }
+    conditions: ['dev'],
+  },
+  test: {
+    threads: false,
+  },
+  // @ts-expect-error vite 5
+  plugins: [Macros()],
 })
