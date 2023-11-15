eb1832af6fedaf2bdd8d733c9f64e4b6ff9ff56d
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2023-11-15 21:16:21 +0800

    refactor(eslint-config): sort keys

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,3 +1,4 @@
+/*eslint sort-keys/sort-keys-fix: "error"*/
 // @ts-check
 
 /** @type import('eslint-define-config').ESLintConfig */
@@ -5,13 +6,13 @@ const config = {
   globals: {
     $: 'readonly',
     $$: 'readonly',
-    $ref: 'readonly',
-    $shallowRef: 'readonly',
     $computed: 'readonly',
     $customRef: 'readonly',
     $defineModels: 'readonly',
     $defineProps: 'readonly',
     $definePropsRefs: 'readonly',
+    $ref: 'readonly',
+    $shallowRef: 'readonly',
     $toRef: 'readonly',
     defineEmit: 'readonly',
     defineModels: 'readonly',
