25e01ff15eec9549a85ef463809a9a24ca0e65c9
gaokefei <260480378@qq.com>
2023-12-14 02:45:39 +0800

    feat(better-define): support short-emits

---
+++ playground/vue3/src/examples/better-define/short-emits.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+type ShortEmits = {
+  click: []
+}
+
+const emit = defineEmits<ShortEmits>()
+</script>
+
+<template>
+  <button @click="emit('click')">click</button>
+</template>
