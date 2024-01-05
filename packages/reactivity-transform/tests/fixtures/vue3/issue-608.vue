2778e1694308ad076fe82df183af84a7bbde5a50
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2024-01-06 01:00:16 +0800

    test: add unit test

---
+++ packages/reactivity-transform/tests/fixtures/vue3/issue-608.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { isRef } from 'vue'
+
+let ref = $ref('value')
+console.log(isRef(ref))
+
+try {
+} catch {
+  const ref = ''
+}
+
+console.log(isRef(ref))
+</script>
