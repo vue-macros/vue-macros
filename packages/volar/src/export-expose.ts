201a978cc0302736d1e07b6e55d6e2ff20d1c197
gaokefei <260480378@qq.com>
2024-03-21 03:06:45 +0800

    feat(volar/export-expose): support export `function` & `class` & `module`

--- packages/volar/src/export-expose.ts
+++ packages/volar/src/export-expose.ts
@@ -31,26 +31,131 @@ function transform({
   )
   if (!filter(fileName)) return
 
-  const exposed: Record<string, Segment<FileRangeCapabilities>> = Object.create(
-    null,
-  )
+  const exposed: Record<string, string> = Object.create(null)
   for (const stmt of sfc.scriptSetup!.ast.statements) {
-    if (!ts.isVariableStatement(stmt)) continue
-    const exportModifier = stmt.modifiers?.find(
-      (m) => m.kind === ts.SyntaxKind.ExportKeyword,
-    )
-    if (!exportModifier) continue
+    if (ts.isExportDeclaration(stmt) && stmt.exportClause) {
+      if (ts.isNamedExports(stmt.exportClause)) {
+        const exportMap = new Map<
+          Segment<FileRangeCapabilities>,
+          Segment<FileRangeCapabilities>
+        >()
+        stmt.exportClause.elements.forEach((element) => {
+          if (element.isTypeOnly) return
 
-    const start = exportModifier.getStart(sfc.scriptSetup?.ast)
-    const end = exportModifier.getEnd()
-    replaceSourceRange(file.content, 'scriptSetup', start, end)
+          const name = element.name
+          const propertyName = element.propertyName || name
 
-    for (const decl of stmt.declarationList.declarations) {
-      if (!ts.isIdentifier(decl.name)) continue
-      const name = decl.name.text
-      const start = decl.name.getStart(sfc.scriptSetup?.ast)
+          exportMap.set(
+            [
+              propertyName.text,
+              'scriptSetup',
+              propertyName.getStart(sfc.scriptSetup?.ast),
+              FileRangeCapabilities.full,
+            ],
+            [
+              name.text,
+              'scriptSetup',
+              name.getStart(sfc.scriptSetup?.ast),
+              FileRangeCapabilities.full,
+            ],
+          )
 
-      exposed[name] = [name, 'scriptSetup', start, FileRangeCapabilities.full]
+          exposed[name.text] = propertyName.text
+        })
+
+        if (stmt.moduleSpecifier) {
+          const start = stmt.getStart(sfc.scriptSetup?.ast)
+          const end = stmt.getEnd()
+
+          replaceSourceRange(
+            file.content,
+            'scriptSetup',
+            start,
+            start + 6,
+            'import',
+          )
+          replaceSourceRange(
+            file.content,
+            'scriptSetup',
+            end,
+            end,
+            `;[${Array.from(exportMap.values()).map(([name]) => name)}];`,
+          )
+        } else {
+          replaceSourceRange(
+            file.content,
+            'scriptSetup',
+            stmt.getStart(sfc.scriptSetup?.ast),
+            stmt.getEnd(),
+            `(({`,
+            ...Array.from(exportMap.entries()).flatMap(([name, value]) =>
+              name[0] === value[0] ? [value, ','] : [name, ':', value, ','],
+            ),
+            `})=>{${Array.from(exportMap.values()).map(([name]) => name)}`,
+            `})({${Array.from(exportMap.keys()).map(([name]) => name)}})`,
+          )
+        }
+      } else if (ts.isNamespaceExport(stmt.exportClause)) {
+        const start = stmt.getStart(sfc.scriptSetup?.ast)
+        const end = stmt.getEnd()
+
+        replaceSourceRange(
+          file.content,
+          'scriptSetup',
+          start,
+          start + 6,
+          'import',
+        )
+        replaceSourceRange(
+          file.content,
+          'scriptSetup',
+          end,
+          end,
+          `;[${stmt.exportClause.name.text}];`,
+        )
+      }
+    } else if (
+      ts.isVariableStatement(stmt) ||
+      ts.isFunctionDeclaration(stmt) ||
+      ts.isClassDeclaration(stmt)
+    ) {
+      const exportModifier = stmt.modifiers?.find(
+        (m) => m.kind === ts.SyntaxKind.ExportKeyword,
+      )
+      if (!exportModifier) continue
+
+      const exposedValues: string[] = []
+      if (ts.isVariableStatement(stmt)) {
+        for (const decl of stmt.declarationList.declarations) {
+          if (!decl.name) continue
+
+          if (ts.isIdentifier(decl.name)) {
+            const name = decl.name.text
+            exposed[name] = name
+          } else if (ts.isObjectBindingPattern(decl.name)) {
+            decl.name.elements.forEach((element) => {
+              if (!ts.isIdentifier(element.name)) return
+
+              exposedValues.push(element.name.text)
+              exposed[element.name.text] =
+                element.propertyName && ts.isIdentifier(element.propertyName)
+                  ? element.propertyName.text
+                  : element.name.text
+            })
+          }
+        }
+      } else if (stmt.name && ts.isIdentifier(stmt.name)) {
+        const name = stmt.name.text
+        exposed[name] = name
+      }
+
+      replaceSourceRange(
+        file.content,
+        'scriptSetup',
+        exportModifier.getStart(sfc.scriptSetup?.ast),
+        exportModifier.getEnd(),
+        exposedValues.length > 0 ? `[${exposedValues}];` : '',
+      )
     }
   }
 
