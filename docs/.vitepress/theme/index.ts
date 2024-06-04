a54a87bb9ad38f51e9df0ab2b53713d3e7a34b00
XiaoMouz <gxiaomouz@gmail.com>
2024-06-05 00:02:27 +0800

    feat(docs): add read enhanced plugins (#695)
    Co-authored-by: Rizumu Ayaka <rizumu@ayaka.moe>
    Co-authored-by: Neko Ayaka <neko@ayaka.moe>

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,10 +1,12 @@
 import Theme from 'vitepress/theme'
+import { NolebaseGitChangelogPlugin } from '@nolebase/vitepress-plugin-git-changelog/client'
 import WarnBadge from '../components/WarnBadge.vue'
 import StabilityLevel from '../components/StabilityLevel.vue'
 import Layout from './Layout.vue'
 import type { EnhanceAppContext } from 'vitepress'
 import 'uno.css'
 import './style.css'
+import '@nolebase/vitepress-plugin-git-changelog/client/style.css'
 
 export default {
   ...Theme,
@@ -12,5 +14,17 @@ export default {
   enhanceApp({ app }: EnhanceAppContext) {
     app.component('WarnBadge', WarnBadge)
     app.component('StabilityLevel', StabilityLevel)
+    app.use(NolebaseGitChangelogPlugin, {
+      mapAuthors: [
+        {
+          name: 'Kevin Deng',
+          mapByNameAliases: [
+            '三咲智子 Kevin Deng',
+            '三咲智子',
+            '三咲智子 (Kevin)',
+          ],
+        },
+      ],
+    })
   },
 }
