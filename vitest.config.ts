fbc964ff3eb2f58a032b655b9c2bb09a7188f9d8
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2024-06-06 23:00:54 +0800

    chore: use dot test reporter by default

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,12 @@
+import process from 'node:process'
 import path from 'node:path'
 import { defineConfig } from 'vitest/config'
 import Macros from 'unplugin-macros/vite'
 
 export default defineConfig({
+  test: {
+    reporters: process.env.GITHUB_ACTIONS ? ['dot', 'github-actions'] : ['dot'],
+  },
   resolve: {
     alias: {
       '#macros': path.resolve(__dirname, 'macros/index.ts'),
