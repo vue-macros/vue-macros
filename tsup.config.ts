94b8f07807300a5e6d16a395d0e24dda4d2eac74
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2024-07-29 13:47:08 +0800

    feat: enable `noCheck` for dts

--- tsup.config.ts
+++ tsup.config.ts
@@ -21,6 +21,7 @@ export default defineConfig({
           compilerOptions: {
             composite: false,
             customConditions: [],
+            noCheck: true,
           },
         },
   tsconfig: '../../tsconfig.lib.json',
