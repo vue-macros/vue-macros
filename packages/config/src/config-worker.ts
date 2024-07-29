e76993dc5a25a27b438185ece35cd7aa172e406f
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2024-07-29 22:59:17 +0800

    refactor config loader

--- packages/config/src/config-worker.ts
+++ packages/config/src/config-worker.ts
@@ -1,26 +1,13 @@
-import { readFile } from 'node:fs/promises'
 import { loadConfig } from 'unconfig'
 import type { Options } from './options'
 
 export async function loadConfigAsync(cwd: string): Promise<Options> {
-  const { importx } = await import('importx')
   const { config } = await loadConfig<Options>({
     sources: [
       {
         files: 'vue-macros.config',
         extensions: ['mts', 'cts', 'ts', 'mjs', 'cjs', 'js', 'json', ''],
-        async parser(filepath) {
-          try {
-            return JSON.parse(await readFile(filepath, 'utf-8'))
-          } catch {
-            const result = await importx(filepath, {
-              parentURL: filepath,
-              cache: false,
-              loader: 'jiti',
-            })
-            return result.default || result
-          }
-        },
+        loader: 'jiti',
       },
       {
         files: 'package.json',
