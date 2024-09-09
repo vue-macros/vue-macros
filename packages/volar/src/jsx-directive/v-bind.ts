06aed122614673e48ab3596155dc61b68484fcfd
Blake Newman <blake.newman@askattest.com>
2024-09-09 11:51:27 +0100

    fix(volar/jsx-directive): incorrect transformation of intrinsic attributes (#801)
    * fix(volar): incorrect transformation of intrinsic attributes
    
    Vue components can accept intrinsic attribtues such as `aria-*` or
    `data-*`. The current implementation transforms all attributes with
    a `-` character. We should ignore any attributes for v-bind if they
    don't contain `_` as the purpose of the tansformation is to
    support modifiers.
    
    * Update index.ts
    
    * Update v-bind.ts
    
    * Create two-poems-jump.md
    
    * Update v-bind.ts
    
    ---------
    
    Co-authored-by: zhiyuanzmj <32807958+zhiyuanzmj@users.noreply.github.com>

--- packages/volar/src/jsx-directive/v-bind.ts
+++ packages/volar/src/jsx-directive/v-bind.ts
@@ -1,4 +1,3 @@
-import { allCodeFeatures, type Code } from '@vue/language-core'
 import { replaceSourceRange } from 'muggle-string'
 import { getStart, getText } from '../common'
 import type { JsxDirective, TransformOptions } from './index'
@@ -9,33 +8,15 @@ export function transformVBind(
 ): void {
   if (nodes.length === 0) return
 
-  const { codes, ts, source } = options
+  const { codes, source } = options
 
   for (const { attribute } of nodes) {
     const attributeName = getText(attribute.name, options)
     const start = getStart(attribute.name, options)
     const end = attribute.name.end
 
-    if (
-      attributeName.includes('-') &&
-      attribute.initializer &&
-      !ts.isStringLiteral(attribute.initializer)
-    ) {
-      replaceSourceRange(
-        codes,
-        source,
-        start,
-        end,
-        ...(attributeName
-          .split('_')[0]
-          .split('-')
-          .map((code, index, codes) => [
-            index ? code.at(0)?.toUpperCase() + code.slice(1) : code,
-            source,
-            start + (index && codes[index - 1].length + 1),
-            allCodeFeatures,
-          ]) as Code[]),
-      )
+    if (attributeName.includes('_')) {
+      replaceSourceRange(codes, source, start + attributeName.indexOf('_'), end)
     }
   }
 }
