ec400594ec89dc97f2aab94d45206287fc7b596f
zhiyuanzmj <32807958+zhiyuanzmj@users.noreply.github.com>
2024-09-09 18:40:49 +0800

    Update v-bind.ts

--- packages/volar/src/jsx-directive/v-bind.ts
+++ packages/volar/src/jsx-directive/v-bind.ts
@@ -9,34 +9,15 @@ export function transformVBind(
 ): void {
   if (nodes.length === 0) return
 
-  const { codes, ts, source } = options
+  const { codes, source } = options
 
   for (const { attribute } of nodes) {
     const attributeName = getText(attribute.name, options)
     const start = getStart(attribute.name, options)
     const end = attribute.name.end
 
-    if (
-      attributeName.includes('-') &&
-      attributeName.includes('_') &&
-      attribute.initializer &&
-      !ts.isStringLiteral(attribute.initializer)
-    ) {
-      replaceSourceRange(
-        codes,
-        source,
-        start,
-        end,
-        ...(attributeName
-          .split('_')[0]
-          .split('-')
-          .map((code, index, codes) => [
-            index ? code.at(0)?.toUpperCase() + code.slice(1) : code,
-            source,
-            start + (index && codes[index - 1].length + 1),
-            allCodeFeatures,
-          ]) as Code[]),
-      )
+    if (attributeName.includes('_')) {
+      replaceSourceRange(codes, source, start + attributeName.indexOf('_'), end)
     }
   }
 }
