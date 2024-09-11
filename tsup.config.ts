d90ac0c3ab375762df038aca3e14a858d02a0134
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2024-09-11 09:45:32 +0800

    feat: bundle dts

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,6 +1,10 @@
+import { copyFile, readFile, rm, writeFile } from 'node:fs/promises'
 import path from 'node:path'
 import process from 'node:process'
 import { fileURLToPath } from 'node:url'
+import fg from 'fast-glob'
+import { rollup } from 'rollup'
+import dts from 'rollup-plugin-dts'
 import { defineConfig, type Options } from 'tsup'
 import { createUnplugin } from 'unplugin'
 import { IsolatedDecl } from 'unplugin-isolated-decl'
@@ -10,6 +14,9 @@ import { Unused, type Options as UnusedOptions } from 'unplugin-unused'
 
 const filename = fileURLToPath(import.meta.url)
 const macros = path.resolve(filename, '../macros')
+
+const pkg = JSON.parse(await readFile('./package.json', 'utf8'))
+const isESM = pkg.type === 'module'
 
 export function config({
   ignoreDeps = { peerDependencies: ['vue'] },
@@ -24,8 +31,10 @@ export function config({
   splitting?: boolean
   onlyIndex?: boolean
 } = {}): Options {
+  const entry = onlyIndex ? ['./src/index.ts'] : ['./src/*.ts', '!./**.d.ts']
+
   return {
-    entry: onlyIndex ? ['./src/index.ts'] : ['./src/*.ts', '!./**.d.ts'],
+    entry,
     format: ['cjs', 'esm'],
     target: 'node16.14',
     splitting,
@@ -55,6 +64,7 @@ export function config({
             {
               exclude: [/node_modules/, `${macros}/**`],
               transformer: 'typescript',
+              extraOutdir: 'temp',
             },
             meta,
           ),
@@ -83,6 +93,49 @@ export function config({
         ]
       }).esbuild(),
     ],
+
+    async onSuccess() {
+      const entryFiles = await fg(entry)
+      const input = entryFiles.map((file) =>
+        path.resolve(
+          process.cwd(),
+          file.replace('src', 'dist/temp').replace(/\.ts$/, '.d.ts'),
+        ),
+      )
+      const build = await rollup({
+        input,
+        plugins: [dts()],
+        external(id) {
+          return id[0] !== '.' && id[0] !== '/'
+        },
+      })
+
+      await build.write({
+        dir: 'dist',
+        format: 'es',
+        entryFileNames: (chunk) => {
+          let filename = chunk.name
+          if (!filename.endsWith('.d')) filename += '.d'
+          return `${filename}.ts`
+        },
+      })
+
+      for (const file of entryFiles) {
+        const mainFormat = file.replace('src', 'dist').replace(/\.ts$/, '.d.ts')
+        const alterFormat = mainFormat.replace(/\.ts$/, isESM ? '.cts' : '.mts')
+        await copyFile(mainFormat, alterFormat)
+
+        const cjsFormat = isESM ? alterFormat : mainFormat
+
+        const cjsExport = (await readFile(cjsFormat, 'utf8')).replace(
+          /(?<=(?:[;}]|^)\s*export\s*)\{\s*([\w$]+)\s*as\s+default\s*\}/,
+          `= $1`,
+        )
+        await writeFile(cjsFormat, cjsExport, 'utf8')
+      }
+
+      await rm(path.resolve(process.cwd(), 'dist/temp'), { recursive: true })
+    },
   }
 }
 
