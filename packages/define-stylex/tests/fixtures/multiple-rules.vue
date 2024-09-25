8867384422712f2fcf9dcd02e65ef8df1221ffb6
BeiyanYunyi <14120445+BeiyanYunyi@users.noreply.github.com>
2024-09-25 14:02:57 +0800

    test(define-stylex): add 2 test cases

---
+++ packages/define-stylex/tests/fixtures/multiple-rules.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const styles = defineStyleX({
+  red: { color: 'red' },
+  bold: { fontWeight: 'bold' },
+})
+</script>
+
+<template>
+  <p><span v-stylex="(styles.red, styles.bold)">Red</span> Text</p>
+</template>
