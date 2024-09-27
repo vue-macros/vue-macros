3af36d1dcda518312bf7980b122452ecd4554b93
zhiyuanzmj <260480378@qq.com>
2024-09-27 22:55:10 +0800

    fix: format

--- packages/jsx-macros/tests/fixtures/define-component.tsx
+++ packages/jsx-macros/tests/fixtures/define-component.tsx
@@ -1,14 +1,14 @@
 import { defineComponent } from 'vue'
 
 const Comp = defineComponent(
-  ({ bar, ...props }: {bar: 'bar', baz: 'baz'}) => {
+  ({ bar, ...props }: { bar: 'bar'; baz: 'baz' }) => {
     const foo = defineModel('foo')
     return <div>{[foo.value, bar, props.baz]}</div>
   },
   { inheritAttrs: false },
 )
 
-const Comp1 = defineComponent((props: {bar: 'bar'}) => {
+const Comp1 = defineComponent((props: { bar: 'bar' }) => {
   const foo = defineModel('foo')
   return <div>{[foo.value, props['bar']]}</div>
 })
