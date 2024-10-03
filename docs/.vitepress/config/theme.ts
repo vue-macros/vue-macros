994a2b92d4fb0c939915228230c2f3b4d8a76b8f
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2024-10-04 03:40:46 +0800

    docs: update copyright

--- docs/.vitepress/config/theme.ts
+++ docs/.vitepress/config/theme.ts
@@ -308,7 +308,7 @@ export function getLocaleConfig(lang: string) {
     footer: {
       message: t('Made with ❤️'),
       copyright:
-        'MIT License © 2022-PRESENT <a href="https://github.com/sxzz">三咲智子 Kevin Deng</a>',
+        'MIT License © 2022-PRESENT<br /><a href="https://github.com/sxzz">三咲智子 Kevin Deng</a> & Vue Macros Contributors',
     },
     editLink: {
       pattern: `${githubLink}/edit/main/docs/:path`,
