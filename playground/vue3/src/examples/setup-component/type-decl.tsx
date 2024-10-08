1979891c3a038c26feeb0719c74bfe5031458ec6
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2024-10-08 17:36:06 +0800

    chore(playground): cleanup console

--- playground/vue3/src/examples/setup-component/type-decl.tsx
+++ playground/vue3/src/examples/setup-component/type-decl.tsx
@@ -1,7 +1,9 @@
 import { computed, ref } from 'vue'
 import { foo } from './foo'
 
-console.log(foo)
+if (foo !== 1) {
+  throw new Error('unexpected value')
+}
 
 export const SetupComponentTypeDecl: SetupFC = () => {
   const count = ref(0)
