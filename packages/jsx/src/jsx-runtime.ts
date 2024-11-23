3bfdbae985810d67b16f7534d2cfe6692e69e457
zhiyuanzmj <260480378@qq.com>
2024-11-23 12:14:38 +0800

    fix: mono

---
+++ packages/jsx/src/jsx-runtime.ts
@@ -0,0 +1,39 @@
+import { Fragment, h } from 'vue'
+import type { NativeElements } from './jsx-runtime/dom'
+
+function jsx(type: any, props: any, key: any): ReturnType<typeof h> {
+  const { children } = props
+  delete props.children
+  if (arguments.length > 2) {
+    props.key = key
+  }
+  return h(type, props, children)
+}
+
+export { Fragment, jsx, jsx as jsxs, jsx as jsxDEV }
+
+type VNode = import('vue').VNode
+type ReservedProps = import('vue').ReservedProps
+type AllowedComponentProps = import('vue').AllowedComponentProps
+type ComponentCustomProps = import('vue').ComponentCustomProps
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace JSX {
+    interface Element extends VNode {}
+    interface ElementClass {
+      $props: {}
+    }
+    interface ElementAttributesProperty {
+      $props: {}
+    }
+    interface IntrinsicElements extends NativeElements {
+      [name: string]: any
+    }
+
+    interface IntrinsicAttributes
+      extends ReservedProps,
+        AllowedComponentProps,
+        ComponentCustomProps {}
+  }
+}
--- packages/jsx/src/jsx-runtime/global.d.ts
+++ packages/jsx/src/jsx-runtime/global.d.ts
@@ -1,11 +1,4 @@
-/*
-React projects that don't include the DOM library need these interfaces to compile.
-React Native applications use React, but there is no DOM available. The JavaScript runtime
-is ES6/ES2015 only. These definitions allow such projects to compile with only `--lib ES6`.
-
-Warning: all of these interfaces are empty. If you want type definitions for various properties
-(such as HTMLInputElement.prototype.value), you need to add `--lib DOM` (via command line or tsconfig.json).
-*/
+// Copied from https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/react/global.d.ts
 
 interface Event {}
 interface AnimationEvent extends Event {}
