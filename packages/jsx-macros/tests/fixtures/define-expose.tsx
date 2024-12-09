4b7bfa7506bb5cb1ed9b1d3c1a9134d20d18d316
zhiyuanzmj <260480378@qq.com>
2024-12-09 11:34:46 +0800

    fix: lint

--- packages/jsx-macros/tests/fixtures/define-expose.tsx
+++ packages/jsx-macros/tests/fixtures/define-expose.tsx
@@ -1,18 +1,18 @@
-export function Comp () {
+export function Comp() {
   defineExpose({
     foo: 1,
   })
   return <div />
 }
 
-export const Comp1 = function(_props: any){
+export const Comp1 = function (_props: any) {
   defineExpose({
     foo: 1,
   })
   return <div />
 }
 
-export const Comp2 = ({ref: _ref}: any)=>{
+export const Comp2 = ({ ref: _ref }: any) => {
   defineExpose({
     foo: 1,
   })
