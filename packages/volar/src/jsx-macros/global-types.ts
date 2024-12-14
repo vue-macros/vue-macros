852a606d41f22e0be255502aa947589a6b7e63db
zhiyuanzmj <260480378@qq.com>
2024-12-14 15:38:20 +0800

    feat: support vue-sfc

--- packages/volar/src/jsx-macros/global-types.ts
+++ packages/volar/src/jsx-macros/global-types.ts
@@ -4,11 +4,12 @@ import type { TransformOptions } from '.'
 export function getGlobalTypes(options: TransformOptions): string {
   return `
 const { defineModel${options.defineModel.alias[0] !== 'defineModel' ? `: ${options.defineModel.alias[0]}` : ''} } = await import('vue')
+declare function ${HELPER_PREFIX}${options.defineComponent.alias[0]}<T extends ((props?: any) => any)>(setup: T, options?: Pick<import('vue').ComponentOptions, 'name' | 'inheritAttrs' | 'components' | 'directives'> & { props?: import('vue').ComponentObjectPropsOptions }): T;
 declare function ${options.defineSlots.alias[0]}<T extends Record<string, any>>(): Partial<T>;
 declare function ${options.defineSlots.alias[0]}<T extends Record<string, any>>(slots: T): T;
 declare function ${options.defineExpose.alias[0]}<Exposed extends Record<string, any> = Record<string, any>>(exposed?: Exposed): Exposed;
-type __VLS_StyleArgs = [style: string, options?: { scoped?: boolean }];
-declare const ${options.defineStyle.alias[0]}: { <T>(...args: __VLS_StyleArgs): T; scss: <T>(...args: __VLS_StyleArgs)=> T; sass: <T>(...args: __VLS_StyleArgs)=> T; stylus: <T>(...args: __VLS_StyleArgs)=> T; less: <T>(...args: __VLS_StyleArgs)=> T; postcss: <T>(...args: __VLS_StyleArgs)=> T };
-declare function ${HELPER_PREFIX}${options.defineComponent.alias[0]}<T extends ((props?: any) => any)>(setup: T, options?: Pick<import('vue').ComponentOptions, 'name' | 'inheritAttrs' | 'components' | 'directives'> & { props?: import('vue').ComponentObjectPropsOptions }): T;
+declare const ${options.defineStyle.alias[0]}: { <T>(...args: ${HELPER_PREFIX}StyleArgs): T; scss: <T>(...args: ${HELPER_PREFIX}StyleArgs)=> T; sass: <T>(...args: ${HELPER_PREFIX}StyleArgs)=> T; stylus: <T>(...args: ${HELPER_PREFIX}StyleArgs)=> T; less: <T>(...args: ${HELPER_PREFIX}StyleArgs)=> T; postcss: <T>(...args: ${HELPER_PREFIX}StyleArgs)=> T };
+type ${HELPER_PREFIX}StyleArgs = [style: string, options?: { scoped?: boolean }];
+type ${HELPER_PREFIX}PrettifyLocal<T> = { [K in keyof T]: T[K]; } & {};
 `
 }
