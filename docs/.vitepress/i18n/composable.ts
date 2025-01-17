8b4681eb07ca74817928b6ea27195ba2393b7f48
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-01-18 05:58:43 +0800

    fix(docs): re-render on changing language

--- docs/.vitepress/i18n/composable.ts
+++ docs/.vitepress/i18n/composable.ts
@@ -2,5 +2,6 @@ import { useData } from 'vitepress'
 import { t } from './utils'
 
 export function useTranslate(lang?: string) {
-  return (key: string) => t(key, lang || useData().lang.value)
+  const { lang: vpLang } = useData()
+  return (key: string) => t(key, lang || vpLang.value)
 }
