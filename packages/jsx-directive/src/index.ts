bed1e0bf4b9eb2e71d5ff513ad065dbf013f80ea
zhiyuanzmj <260480378@qq.com>
2025-01-28 09:25:53 +0800

    fix: typecheck

--- packages/jsx-directive/src/index.ts
+++ packages/jsx-directive/src/index.ts
@@ -1,6 +1,8 @@
 import { createUnplugin, type UnpluginInstance } from 'unplugin'
 import transformJsxDirective, { type Options } from './api'
 
+export { type Options }
+
 const plugin: UnpluginInstance<Options | undefined, false> = createUnplugin(
   transformJsxDirective,
 )
