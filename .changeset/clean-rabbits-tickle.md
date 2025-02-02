f091389c247e253933fd38c17d840e6fc3900c91
zhiyuanzmj <32807958+zhiyuanzmj@users.noreply.github.com>
2025-02-02 22:51:33 +0800

    feat(jsx-directive): expose raw to support browser environments (#906)

---
+++ .changeset/clean-rabbits-tickle.md
@@ -0,0 +1,6 @@
+---
+"@vue-macros/jsx-directive": patch
+---
+
+expose raw to support browser environments
+  
--- packages/jsx-directive/src/index.ts
+++ packages/jsx-directive/src/index.ts
@@ -1,65 +1,8 @@
-import {
-  createFilter,
-  detectVueVersion,
-  FilterFileType,
-  getFilterPattern,
-  REGEX_NODE_MODULES,
-  REGEX_SETUP_SFC,
-  type BaseOptions,
-  type MarkRequired,
-} from '@vue-macros/common'
-import { generatePluginName } from '#macros' with { type: 'macro' }
-import {
-  createUnplugin,
-  type UnpluginContextMeta,
-  type UnpluginInstance,
-} from 'unplugin'
-import { transformJsxDirective } from './core'
+import { createUnplugin, type UnpluginInstance } from 'unplugin'
+import { plugin, type Options, type OptionsResolved } from './core/plugin'
 
-export type Options = BaseOptions & {
-  prefix?: string
-  lib?: 'vue' | 'vue/vapor' | (string & {})
-}
-export type OptionsResolved = MarkRequired<
-  Options,
-  'version' | 'prefix' | 'lib'
->
+export type { Options, OptionsResolved }
 
-function resolveOptions(
-  options: Options,
-  framework: UnpluginContextMeta['framework'],
-): OptionsResolved {
-  const version = options.version || detectVueVersion(undefined, 0)
-  const include = getFilterPattern(
-    [FilterFileType.VUE_SFC, FilterFileType.SRC_FILE],
-    framework,
-  )
-  return {
-    include,
-    exclude: [REGEX_NODE_MODULES, REGEX_SETUP_SFC],
-    ...options,
-    prefix: options.prefix ?? 'v-',
-    lib: options.lib ?? 'vue',
-    version,
-  }
-}
-
-const name = generatePluginName()
-
-const plugin: UnpluginInstance<Options | undefined, false> = createUnplugin(
-  (userOptions = {}, { framework }) => {
-    const options = resolveOptions(userOptions, framework)
-    const filter = createFilter(options)
-
-    return {
-      name,
-      enforce: 'pre',
-
-      transformInclude: filter,
-      transform(code, id) {
-        return transformJsxDirective(code, id, options)
-      },
-    }
-  },
-)
-export default plugin
+const unplugin: UnpluginInstance<Options | undefined, false> =
+  createUnplugin(plugin)
+export default unplugin
