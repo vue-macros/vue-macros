443737eef58a4c453e52a781ab31b9e6790cbeee
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-02-06 14:11:08 +0800

    refactor(jsx-directive)!: drop vue 2 support

--- packages/jsx-directive/tests/v-for.test.ts
+++ packages/jsx-directive/tests/v-for.test.ts
@@ -2,24 +2,8 @@ import { testFixtures } from '@vue-macros/test-utils'
 import { describe } from 'vitest'
 import { transformJsxDirective } from '../src/api'
 
-describe('jsx-vue-directive', () => {
-  describe('vue 2.7 v-for', async () => {
-    await testFixtures(
-      import.meta.glob<string>('./fixtures/v-for/*.{vue,jsx,tsx}', {
-        eager: true,
-        query: '?raw',
-        import: 'default',
-      }),
-      (_, id, code) =>
-        transformJsxDirective(code, id, {
-          version: 2.7,
-          lib: 'vue',
-          prefix: 'v-',
-        })?.code,
-    )
-  })
-
-  describe('vue 3 v-for', async () => {
+describe('v-for', () => {
+  describe('vue', async () => {
     await testFixtures(
       import.meta.glob<string>('./fixtures/v-for/*.{vue,jsx,tsx}', {
         eager: true,
@@ -35,7 +19,7 @@ describe('jsx-vue-directive', () => {
     )
   })
 
-  describe('vue/vapor v-for', async () => {
+  describe('vue/vapor', async () => {
     await testFixtures(
       import.meta.glob<string>('./fixtures/v-for/*.{vue,jsx,tsx}', {
         eager: true,
