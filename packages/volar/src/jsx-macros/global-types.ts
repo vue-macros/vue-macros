36daec144f759062ec6c988bc715758e02022b69
zhiyuanzmj <260480378@qq.com>
2025-02-27 10:15:00 +0800

    fix: global-types

--- packages/volar/src/jsx-macros/global-types.ts
+++ packages/volar/src/jsx-macros/global-types.ts
@@ -2,17 +2,41 @@ import { HELPER_PREFIX } from '@vue-macros/common'
 import type { TransformOptions } from '.'
 
 export function getGlobalTypes(options: TransformOptions): string {
-  const defineComponent = options.defineComponent.alias.map(
-    (alias) =>
-      `declare function ${HELPER_PREFIX}${alias}<T extends ((props?: any) => any)>(setup: T, options?: Pick<import('vue').ComponentOptions, 'name' | 'inheritAttrs' | 'components' | 'directives'> & { props?: import('vue').ComponentObjectPropsOptions }): T;\n`,
-  )
+  const defineComponent = options.defineComponent.alias
+    .map(
+      (alias) =>
+        `declare function ${HELPER_PREFIX}${alias}<T extends ((props?: any) => any)>(setup: T, options?: Pick<import('vue').ComponentOptions, 'name' | 'inheritAttrs' | 'components' | 'directives'> & { props?: import('vue').ComponentObjectPropsOptions }): T;\n`,
+    )
+    .join('')
+  const defineSlots = options.defineSlots.alias
+    .flatMap((alias) => [
+      `declare function ${alias}<T extends ((props?: any) => any)>(setup: T, options?: Pick<import('vue').ComponentOptions, 'name' | 'inheritAttrs' | 'components' | 'directives'> & { props?: import('vue').ComponentObjectPropsOptions }): T;\n`,
+      `declare function ${alias}<T extends Record<string, any>>(slots: T): T;\n`,
+    ])
+    .join('')
+  const defineExpose = options.defineExpose.alias
+    .map(
+      (alias) =>
+        `declare function ${alias}<Exposed extends Record<string, any> = Record<string, any>>(exposed?: Exposed): Exposed;`,
+    )
+    .join('')
+  const defineStyle = options.defineStyle.alias
+    .map(
+      (alias) =>
+        `declare const ${alias}: { <T>(...args: ${HELPER_PREFIX}StyleArgs): T; scss: <T>(...args: ${HELPER_PREFIX}StyleArgs)=> T; sass: <T>(...args: ${HELPER_PREFIX}StyleArgs)=> T; stylus: <T>(...args: ${HELPER_PREFIX}StyleArgs)=> T; less: <T>(...args: ${HELPER_PREFIX}StyleArgs)=> T; postcss: <T>(...args: ${HELPER_PREFIX}StyleArgs)=> T };\n`,
+    )
+    .join('')
+  const defineModel = options.defineModel.alias
+    .map((alias) =>
+      alias === 'defineModel' ? 'defineModel' : `defineModel: ${alias}`,
+    )
+    .join(', ')
   return `
-const { defineModel${options.defineModel.alias[0] !== 'defineModel' ? `: ${options.defineModel.alias[0]}` : ''} } = await import('vue')
+const { ${defineModel} } = await import('vue')
 ${defineComponent}
-declare function ${options.defineSlots.alias[0]}<T extends Record<string, any>>(): Partial<T>;
-declare function ${options.defineSlots.alias[0]}<T extends Record<string, any>>(slots: T): T;
-declare function ${options.defineExpose.alias[0]}<Exposed extends Record<string, any> = Record<string, any>>(exposed?: Exposed): Exposed;
-declare const ${options.defineStyle.alias[0]}: { <T>(...args: ${HELPER_PREFIX}StyleArgs): T; scss: <T>(...args: ${HELPER_PREFIX}StyleArgs)=> T; sass: <T>(...args: ${HELPER_PREFIX}StyleArgs)=> T; stylus: <T>(...args: ${HELPER_PREFIX}StyleArgs)=> T; less: <T>(...args: ${HELPER_PREFIX}StyleArgs)=> T; postcss: <T>(...args: ${HELPER_PREFIX}StyleArgs)=> T };
+${defineSlots}
+${defineExpose}
+${defineStyle}
 type ${HELPER_PREFIX}StyleArgs = [style: string, options?: { scoped?: boolean }];
 type ${HELPER_PREFIX}PrettifyLocal<T> = { [K in keyof T]: T[K]; } & {};
 type __VLS_IsAny<T> = 0 extends 1 & T ? true : false;
