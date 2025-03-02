0b11b27ffd386c93876aa4fda6f1370d101a05d1
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-03-03 01:53:24 +0800

    feat!(volar): remove template ref

--- packages/volar/src/template-ref.ts
+++
@@ -1,6 +0,0 @@
-// templateRef is Officially implemented in Volar.
-import type { VueLanguagePlugin } from '@vue/language-core'
-
-const plugin: VueLanguagePlugin = () => []
-export default plugin
-export { plugin as 'module.exports' }
