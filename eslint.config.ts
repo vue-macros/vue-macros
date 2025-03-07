a487cf6579b98be9c2cf679bc86c7dbaa461af25
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-03-08 03:17:42 +0800

    chore: upgrade deps

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,52 +2,48 @@
 import { sxzz } from '@sxzz/eslint-config'
 import vueMacros from './packages/eslint-config/src/index.ts'
 
-export default sxzz([
-  vueMacros,
-  {
-    name: 'global-ignores',
-    ignores: ['playground/nuxt', 'playground/astro'],
-  },
-  {
-    name: 'global-rules',
-    rules: {
-      '@typescript-eslint/no-dynamic-delete': 'off',
+export default sxzz()
+  .removeRules('@typescript-eslint/no-dynamic-delete')
+  .append([
+    vueMacros,
+    {
+      name: 'global-ignores',
+      ignores: ['playground/nuxt', 'playground/astro'],
     },
-  },
-  {
-    name: 'markdown',
-    files: ['**/*.md/*.{js,ts,vue}'],
-    rules: {
-      'no-var': 'off',
-      'import/no-mutable-exports': 'off',
-      'import/first': 'off',
+    {
+      name: 'markdown',
+      files: ['**/*.md/*.{js,ts,vue}'],
+      rules: {
+        'no-var': 'off',
+        'import/no-mutable-exports': 'off',
+        'import/first': 'off',
+      },
     },
-  },
-  {
-    name: 'playground/vue3',
-    files: ['playground/vue3/**'],
-    rules: {
-      'no-debugger': 'off',
-      'no-console': 'off',
+    {
+      name: 'playground/vue3',
+      files: ['playground/vue3/**'],
+      rules: {
+        'no-debugger': 'off',
+        'no-console': 'off',
+      },
     },
-  },
-  {
-    name: 'allow-default-export',
-    files: [
-      '**/helper/*',
-      'playground/vue3/**',
-      'packages/{volar,eslint-config}/**',
-      '**/raw.ts',
-    ],
-    rules: {
-      'import/no-default-export': 'off',
+    {
+      name: 'allow-default-export',
+      files: [
+        '**/helper/*',
+        'playground/vue3/**',
+        'packages/{volar,eslint-config}/**',
+        '**/raw.ts',
+      ],
+      rules: {
+        'import/no-default-export': 'off',
+      },
     },
-  },
-  {
-    name: 'sort-config',
-    files: ['**/vue-macros.config.ts'],
-    rules: {
-      'perfectionist/sort-objects': 'error',
+    {
+      name: 'sort-config',
+      files: ['**/vue-macros.config.ts'],
+      rules: {
+        'perfectionist/sort-objects': 'error',
+      },
     },
-  },
-])
+  ])
