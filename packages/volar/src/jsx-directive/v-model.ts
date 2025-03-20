900d13acb82cabe7faf93b24212218104747cdb3
zhiyuanzmj <260480378@qq.com>
2025-03-20 18:56:46 +0800

    fix(volar/jsx-directive): prevent generate empty modifiers

--- packages/volar/src/jsx-directive/v-model.ts
+++ packages/volar/src/jsx-directive/v-model.ts
@@ -127,7 +127,7 @@ function transform(
             ])
           }
 
-          if (!isDynamic) {
+          if (!isDynamic && modifiers.length) {
             modifiersResult.push(
               ` {...{`,
               ...(modifiers.flatMap((modify, index) => [
