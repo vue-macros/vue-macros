937ed2b1fa44192da3cb7f494ae609b76dabd31f
Okinea Dev <hi@okinea.dev>
2025-03-24 19:10:15 +0100

    fix: fix `llms.txt` generation

--- docs/.vitepress/config/index.ts
+++ docs/.vitepress/config/index.ts
@@ -3,6 +3,7 @@ import { transformerTwoslash } from '@shikijs/vitepress-twoslash'
 import ts from 'typescript'
 import { defineConfig } from 'vitepress'
 import { groupIconMdPlugin } from 'vitepress-plugin-group-icons'
+import llmstxt from 'vitepress-plugin-llms'
 import VueMacrosPlugin from 'vue-macros/volar'
 import { docsLink } from '../../../macros'
 import { getLocaleConfig } from './theme'
@@ -69,6 +70,13 @@ export default defineConfig({
             }),
           ]
         : []),
+    ],
+  },
+  vite: {
+    plugins: [
+      llmstxt({
+        ignoreFiles: ['interactive/**/*', 'zh-CN/**/*', 'index.md'],
+      }),
     ],
   },
 })
