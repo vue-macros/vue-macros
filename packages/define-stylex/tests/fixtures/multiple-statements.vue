6bdd4d8617a0f351a86a6f6a032e76a3044edbdb
BeiyanYunyi <14120445+BeiyanYunyi@users.noreply.github.com>
2025-08-19 19:18:41 +0800

    test(define-stylex): add fixture

---
+++ packages/define-stylex/tests/fixtures/multiple-statements.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const a = 'a'
+const stylesa = defineStyleX({ redBold: { color: 'red', fontWeight: 'bold' } }),
+  stylesb = defineStyleX({ redBold: { color: 'red', fontWeight: 'bold' } })
+</script>
+
+<template>
+  <p><span v-stylex="stylesa.redBold">Red</span> <span v-stylex="stylesb.redBold">{{ a }}</span></p>
+</template>
---
+++ packages/define-stylex/tests/fixtures/other-statements.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+// case 1: separated declarations
+const a = 'a'
+// case 2: multiple declarations
+const styles = defineStyleX({ redBold: { color: 'red', fontWeight: 'bold' } }), b = 'b'
+// case 3: separated declarations after
+const c = 'c'
+</script>
+
+<template>
+  <p><span v-stylex="styles.redBold">Red</span> {{ a }} {{ b }} {{ c }}</p>
+</template>
