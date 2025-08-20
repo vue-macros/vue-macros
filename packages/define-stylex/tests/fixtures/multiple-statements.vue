c00a57993bfdf140de19918c3ad5c756685b481e
北雁云依 <14120445+BeiyanYunyi@users.noreply.github.com>
2025-08-20 10:48:39 +0800

    fix(define-stylex): prevent unintended code hoisting(#987)

---
+++ packages/define-stylex/tests/fixtures/multiple-statements.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const a = 'a'
+const stylesA = defineStyleX({ redBold: { color: 'red', fontWeight: 'bold' } }),
+  stylesB = defineStyleX({ redBold: { color: 'red', fontWeight: 'bold' } })
+</script>
+
+<template>
+  <p>
+    <span v-stylex="stylesA.redBold">Red</span>
+    <span v-stylex="stylesB.redBold">{{ a }}</span>
+  </p>
+</template>
